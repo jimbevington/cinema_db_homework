304a6cc2d23abc24401a87e23732be3349a9138d
Jim Bevington <jim.bevington@hotmail.com>
2018-02-11 19:06:28 +0000

    added screening id parameter to ticket class and and hash value to ticket instances in ticket instances in console.rb

--- console.rb
+++ console.rb
@@ -37,13 +37,13 @@ for screening in [screening1, screening2, screening3, screening4, screening5]
   screening.save()
 end
 
-ticket1 = Ticket.new({ 'customer_id' => customer1.id, 'film_id' => film1.id })
-ticket2 = Ticket.new({ 'customer_id' => customer3.id, 'film_id' => film1.id })
-ticket3 = Ticket.new({ 'customer_id' => customer2.id, 'film_id' => film3.id })
-ticket4 = Ticket.new({ 'customer_id' => customer1.id, 'film_id' => film2.id })
-ticket5 = Ticket.new({ 'customer_id' => customer2.id, 'film_id' => film2.id })
-ticket6 = Ticket.new({ 'customer_id' => customer3.id, 'film_id' => film3.id })
-ticket7 = Ticket.new({ 'customer_id' => customer4.id, 'film_id' => film1.id })
+ticket1 = Ticket.new({ 'customer_id' => customer1.id, 'film_id' => film1.id, 'screening_id' => screening1.id })
+ticket2 = Ticket.new({ 'customer_id' => customer3.id, 'film_id' => film1.id, 'screening_id' => screening2.id })
+ticket3 = Ticket.new({ 'customer_id' => customer2.id, 'film_id' => film3.id, 'screening_id' => screening5.id })
+ticket4 = Ticket.new({ 'customer_id' => customer1.id, 'film_id' => film2.id, 'screening_id' => screening3.id })
+ticket5 = Ticket.new({ 'customer_id' => customer2.id, 'film_id' => film2.id, 'screening_id' => screening4.id })
+ticket6 = Ticket.new({ 'customer_id' => customer3.id, 'film_id' => film3.id, 'screening_id' => screening5.id })
+ticket7 = Ticket.new({ 'customer_id' => customer4.id, 'film_id' => film1.id, 'screening_id' => screening2.id })
 
 for ticket in [ticket1, ticket2, ticket3, ticket4, ticket5, ticket6, ticket7]
   ticket.save()
--- models/ticket.rb
+++ models/ticket.rb
@@ -3,12 +3,13 @@ require_relative('../db/sql_runner.rb')
 class Ticket
 
   attr_reader :id
-  attr_accessor :customer_id, :film_id
+  attr_accessor :customer_id, :film_id, :screening_id
 
   def initialize(options)
     @id = options['id'].to_i if options['id']
     @customer_id = options['customer_id']
     @film_id = options['film_id']
+    @screening_id = options['screening_id']
   end
 
   def save()
